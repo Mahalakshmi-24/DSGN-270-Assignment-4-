0c3e8cc5a90bce8030542e82011dd289dbf1a851
Mahalakshmi <mahait07@gmail.com>
2022-03-17 14:46:58 -0600

    files added

---
+++ assets/css/design_system.js
@@ -0,0 +1,22 @@
+const theme = { colors: 
+   { sky_blue: '#3cbedb',
+     white: '#ffffff',
+     red: '#e91717',
+     navy_blue: '#6b9ce5',
+     purrple: '#d6aafa',
+     darkshade_purple: '#c79ee8' },
+  fontSize: 
+   { xs: '0.625rem',
+     sm: '0.8125rem',
+     base: '1rem',
+     lg: '1.25rem',
+     xl: '1.5625rem',
+     '2xl': '1.9375rem',
+     '3xl': '2.1875rem',
+     '4xl': '2.4375rem',
+     '5xl': '3.0625rem' },
+  fontFamily: 
+   { candara: 'Candara',
+     roboto: 'Roboto',
+     philosopher: 'Philosopher' },
+  borderRadius: { none: '0', xs: '0.5rem' } }; export default theme;
